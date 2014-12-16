5b8d7139d116d0225266a8b3ab829b25990b4f22
Ryan Duffy <ryan.duffy@lge.com>
2014-12-16 11:56:41 -0600

    add global configuration block
    Issue: ENYO-594
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- version.js
+++ version.js
@@ -1,9 +1,25 @@
-/**
-	A library of UI widgets designed for use alongside Enyo core in the
-	development of smart TV applications.
+(function (enyo, scope) {
+	/**
+		A library of UI widgets designed for use alongside Enyo core in the
+		development of smart TV applications.
 
-	@namespace moon
-*/
-if (enyo && enyo.version) {
-	enyo.version.moonstone = "2.5.3-zzz.2";
-}
+		@namespace moon
+	*/
+	if (enyo && enyo.version) {
+		enyo.version.moonstone = "2.5.3-zzz.2";
+	}
+
+	if (!scope.moon) {
+		scope.moon = {};
+	}
+
+	/**
+	* Global *design-time* configuration options for Moonstone
+	*
+	* @param {Boolean} accelerate `true` to prefer CSS transforms over position properties
+	* @type {Object}
+	*/
+	scope.moon.config = enyo.mixin({
+		accelerate: true
+	}, scope.moon.config);
+})(enyo, this);
